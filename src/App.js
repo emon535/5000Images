fb8f55a8b450c169acf9b71b063cddb96fddba1d
Foysal Ahmed Emon <emon535@gmail.com>
2019-06-29 17:51:12 +0600

    Added Loader Comploent to wait for data load

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
-import "./App.css";
+
+import { Spinner, Footer } from "reactstrap";
 import { Suspense, lazy } from "react";
+import "./App.css";
+
 const DataTable = lazy(() => import("./Components/DataTable/DataTable"));
 const Images = lazy(() => import("./Components/ImageList/ImageList"));
 
 class App extends Component {
+  _getLoader() {
+    return (
+      <div>
+        <Spinner type="grow" color="primary" />
+        <Spinner type="grow" color="secondary" />
+        <Spinner type="grow" color="success" />
+        <Spinner type="grow" color="danger" />
+        <Spinner type="grow" color="warning" />
+        <Spinner type="grow" color="info" />
+        <Spinner type="grow" color="light" />
+        <Spinner type="grow" color="dark" />
+      </div>
+    );
+  }
   render() {
     return (
       <div className="App">
         <h1 className="heading">Latest Albums</h1>
-        <Suspense fallback="Loading..">
+        <Suspense fallback={this._getLoader()}>
           {/* <Images /> */}
           <DataTable />
         </Suspense>
